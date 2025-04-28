a102cc7eec53992b60db65fb88770ac1d87ea0a9
AkhilaDavulapally <davulapallyakhila@gmail.com>
2025-04-28 20:02:29 +0530

    Create Demo.java

---
+++ Demo.java
@@ -0,0 +1,9 @@
+package com.Exceptions;
+import java.util.*;
+public class Demo {
+	public static void main(String[] args) {
+		System.out.println("Enter age");
+		int age=new Scanner(System.in).nextInt();
+		System.out.println(age);
+	}
+}
