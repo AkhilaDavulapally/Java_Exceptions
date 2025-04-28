e2f780869cfe3accf48fa391012563cbde246b9b
AkhilaDavulapally <davulapallyakhila@gmail.com>
2025-04-28 20:06:51 +0530

    Create Input_Mismatch_Exception.java

---
+++ Input_Mismatch_Exception.java
@@ -0,0 +1,9 @@
+package com.Exceptions;
+import java.util.*;
+public class Input_Mismatch_Exception {
+	public static void main(String[] args) {
+		System.out.println("Enter age");
+		int age=new Scanner(System.in).nextInt();
+		System.out.println(age);
+	}
+}
